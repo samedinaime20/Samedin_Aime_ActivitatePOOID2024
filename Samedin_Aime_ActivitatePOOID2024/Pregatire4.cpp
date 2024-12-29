75d7eb75e832bf7d65f7ab8940193f7779d75f8d
Aime🎀 <samedinaime20@stud.ase.ro>
2024-12-29 23:58:42 +0200

    .

--- Samedin_Aime_ActivitatePOOID2024/Pregatire4.cpp
+++ Samedin_Aime_ActivitatePOOID2024/Pregatire4.cpp
@@ -1,58 +1,58 @@
-#include<iostream>
-using namespace std;
-class Clasa {
-private:
-	const int numar;
-	int nrCopii;
-	int* medie;
-	bool premiant;
-	static string nume;
-public:
-	Clasa() :numar(1) {
-		this->nrCopii = 0;
-		this->medie = nullptr;
-		this->premiant = "false";
-	}
-	Clasa(int nrCopii,
-	int* medie,
-		bool premiant) :numar(20) {
-		this->medie = medie;
-		this->nrCopii = nrCopii;
-		this->premiant = premiant;
-	}
-	~Clasa() {
-		if (this->medie != NULL)
-			delete[]this->medie;
-	}
-	Clasa(const Clasa& c) :numar(c.numar) {
-		this->nrCopii = c.nrCopii;
-		this->premiant = c.premiant;
-		this->medie = new int[nrCopii];
-		for (int i = 0; i < nrCopii; i++)
-			this->medie[i] = c.medie[i];
-	}
-	const Clasa& operator=(const Clasa& e) {
-		if (this != &e) {
-			if (this->medie != NULL) {
-				delete[]this->medie;
-			}
-			this->nrCopii = e.nrCopii;
-			this->premiant = e.premiant;
-			this->medie = new int[nrCopii];
-			for (int i = 0; i < nrCopii; i++)
-				this->medie[i] = e.medie[i];
-		}
-		return *this;
-	}
-	void setNrCopii(int nrCopii){
-		this->nrCopii = nrCopii;
-	}
-	int getNrCopii() {
-		return this->nrCopii;
-	}
-	operator int() {
-		return this->nrCopii;
-	}
-	
-};
-string Clasa::nume = "Clasa 1A";
+//#include<iostream>
+//using namespace std;
+//class Clasa {
+//private:
+//	const int numar;
+//	int nrCopii;
+//	int* medie;
+//	bool premiant;
+//	static string nume;
+//public:
+//	Clasa() :numar(1) {
+//		this->nrCopii = 0;
+//		this->medie = nullptr;
+//		this->premiant = "false";
+//	}
+//	Clasa(int nrCopii,
+//	int* medie,
+//		bool premiant) :numar(20) {
+//		this->medie = medie;
+//		this->nrCopii = nrCopii;
+//		this->premiant = premiant;
+//	}
+//	~Clasa() {
+//		if (this->medie != NULL)
+//			delete[]this->medie;
+//	}
+//	Clasa(const Clasa& c) :numar(c.numar) {
+//		this->nrCopii = c.nrCopii;
+//		this->premiant = c.premiant;
+//		this->medie = new int[nrCopii];
+//		for (int i = 0; i < nrCopii; i++)
+//			this->medie[i] = c.medie[i];
+//	}
+//	const Clasa& operator=(const Clasa& e) {
+//		if (this != &e) {
+//			if (this->medie != NULL) {
+//				delete[]this->medie;
+//			}
+//			this->nrCopii = e.nrCopii;
+//			this->premiant = e.premiant;
+//			this->medie = new int[nrCopii];
+//			for (int i = 0; i < nrCopii; i++)
+//				this->medie[i] = e.medie[i];
+//		}
+//		return *this;
+//	}
+//	void setNrCopii(int nrCopii){
+//		this->nrCopii = nrCopii;
+//	}
+//	int getNrCopii() {
+//		return this->nrCopii;
+//	}
+//	operator int() {
+//		return this->nrCopii;
+//	}
+//	
+//};
+//string Clasa::nume = "Clasa 1A";
--- Samedin_Aime_ActivitatePOOID2024/Pregatire5.cpp
+++ Samedin_Aime_ActivitatePOOID2024/Pregatire5.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
 class Croaziera {
 private:
 	const int idVapor;
@@ -33,8 +34,13 @@ public:
 		this->viteza = c.viteza;
 		this->capacitate = c.capacitate;
 		this->allInclusive = c.allInclusive;
-		this->nationalitate = new char[strlen(nationalitate) + 1];
-		strcpy_s(c.nationalitate, strlen(nationalitate) + 1, this->nationalitate);
+		if (c.nationalitate != nullptr) {
+			this->nationalitate = new char[strlen(c.nationalitate) + 1];
+			strcpy_s(this->nationalitate, strlen(c.nationalitate) + 1, c.nationalitate);
+		}
+		else {
+			this->nationalitate = nullptr;
+		}
 	}
 	const Croaziera& operator=(const Croaziera& e) {
 		if (this != &e) {
@@ -43,8 +49,13 @@ public:
 			this->viteza = e.viteza;
 			this->capacitate = e.capacitate;
 			this->allInclusive = e.allInclusive;
-			this->nationalitate = new char(strlen(nationalitate) + 1);
-			strcpy_s(e.nationalitate, strlen(nationalitate) + 1, this->nationalitate);
+			if (e.nationalitate != nullptr) {
+				this->nationalitate = new char[strlen(e.nationalitate) + 1];
+				strcpy_s(this->nationalitate, strlen(e.nationalitate) + 1, e.nationalitate);
+			}
+			else {
+				this->nationalitate = nullptr;
+			}
 		}
 		return *this;
 	}
@@ -54,16 +65,25 @@ public:
 	void setViteza(float viteza) {
 		this->viteza = viteza;
 	}
+
+	int getCapacitate() {
+		return this->capacitate;
+	}
+	void setCapacitate(int capacitate) {
+		this->capacitate = capacitate;
+	}
 	Croaziera operator+(const Croaziera& p) {
 		Croaziera aux;
-		aux.viteza = this->viteza;
-		aux.capacitate = this->capacitate;
-		aux.allInclusive = this->allInclusive;
+		aux.viteza = this->viteza+p.viteza;
+		aux.capacitate = this->capacitate+p.capacitate;
+		aux.allInclusive = this->allInclusive || p.allInclusive;
 		if (aux.nationalitate != NULL)
 			delete[]aux.nationalitate;
-		if (this->nationalitate != NULL) {
-			aux.nationalitate=new char[strlen(nationalitate) + 1];
-			strcpy_s(aux.nationalitate, strlen(nationalitate) + 1, this->nationalitate);
+		if (this->nationalitate != nullptr || p.nationalitate!=nullptr) {
+			rsize_t length = strlen(this->nationalitate) + strlen(p.nationalitate) + 1;
+			aux.nationalitate = new char[length];
+			strcpy_s(aux.nationalitate, length, this->nationalitate);
+			strcat_s(aux.nationalitate, length, p.nationalitate);
 		}
 		return aux;
 	}
@@ -74,12 +94,17 @@ public:
 
 int main() {
 	Croaziera a1;
+	a1.setViteza(60);
+	a1.setCapacitate(200);
 	Croaziera a2 = a1;
 	Croaziera a3;
+	Croaziera a4;
 	a3 = a1;
-	a3 + a1;
-	a1.setViteza(60);
-	cout << a1.getViteza() << endl;
-
+	a1.setViteza(100);
+	a4=a3+ a1;
+	cout << a1.getViteza() <<" "<<a1.getCapacitate()<< endl;
+	cout<< a2.getViteza() <<" "<<a2.getCapacitate()<< endl;
+	cout << a3.getViteza() <<" "<<a3.getCapacitate()<< endl;
+	cout << a4.getViteza() <<" "<<a4.getCapacitate()<< endl;	
 	return 0;
 }
